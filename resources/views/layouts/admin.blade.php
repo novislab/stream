5c4906fe6389a97d390fa5120fb6ddbab5d0c968
novislab <novislab.dev@gmail.com>
2026-01-27 05:24:26 +0100

    Update admin layout with Flux components

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -1,15 +1,26 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-        <title>{{ $title ?? config('app.name') }}</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <title>{{ $title ?? config('app.name') }}</title>
 
-    </head>
-    <body>
-        {{ $slot }}
-    </body>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+    @fluxAppearance
+</head>
+
+<body class="min-h-screen bg-white dark:bg-zinc-950 antialiased">
+
+    <flux:toast.group>
+        <flux:toast />
+    </flux:toast.group>
+
+    {{ $slot }}
+
+    @fluxScripts
+</body>
+
 </html>
