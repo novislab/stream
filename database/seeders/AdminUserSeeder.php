c04c8aaabf4e27d6d706b4b94fd4bdfa35512410
novislab <novislab.dev@gmail.com>
2026-01-27 10:50:14 +0100

    Add AdminUserSeeder for seeding admin user

---
+++ database/seeders/AdminUserSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+
+class AdminUserSeeder extends Seeder
+{
+    public function run(): void
+    {
+        Role::findOrCreate('admin');
+
+        $admin = User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'admin@example.com',
+            'password' => 'password',
+        ]);
+
+        $admin->assignRole('admin');
+    }
+}
