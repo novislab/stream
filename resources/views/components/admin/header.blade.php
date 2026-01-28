8b30fbb4471b4d699b6681c8ec75ebf08b1adf76
novislab <novislab.dev@gmail.com>
2026-01-28 19:19:12 +0100

    Add admin role check to sidebar and header components

---
+++ resources/views/components/admin/header.blade.php
@@ -0,0 +1,22 @@
+@role('admin')
+<flux:header class="lg:hidden">
+    <flux:sidebar.toggle class="lg:hidden" icon="bars-2" inset="left" />
+
+    <flux:spacer />
+
+    <flux:dropdown position="top" align="start">
+        <flux:profile avatar="/img/demo/user.png" />
+
+        <flux:menu>
+            <flux:menu.radio.group>
+                <flux:menu.radio checked>Olivia Martin</flux:menu.radio>
+                <flux:menu.radio>Truly Delta</flux:menu.radio>
+            </flux:menu.radio.group>
+
+            <flux:menu.separator />
+
+            <flux:menu.item icon="arrow-right-start-on-rectangle">Logout</flux:menu.item>
+        </flux:menu>
+    </flux:dropdown>
+</flux:header>
+@endrole
