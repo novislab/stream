c02fd0d755557c8276351e48d4acd47c15be0d67
novislab <novislab.dev@gmail.com>
2026-01-30 07:05:12 +0100

    Add short URL redirect route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\ShortUrlRedirectController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', fn () => view('welcome'));
+
+Route::get('s/{code}', ShortUrlRedirectController::class)->name('short-url.redirect');
