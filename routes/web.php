5343686b0f9dd0fe3f9ef549665d9e677ee49973
novislab <novislab.dev@gmail.com>
2026-02-01 06:46:22 +0100

    Add Livewire page routes

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ShortUrlRedirectController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', fn () => view('welcome'));
-
 Route::get('s/{code}', ShortUrlRedirectController::class)->name('short-url.redirect');
+Route::livewire('/', 'pages::home')->name('home');
+Route::livewire('/register', 'pages::register')->name('register');
+Route::livewire('/payment', 'pages::payment')->name('payment');
