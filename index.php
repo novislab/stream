35da57e5c8d62f0ba3a48a13d3864bca01c5b775
novislab <novislab.dev@gmail.com>
2026-02-01 08:54:21 +0100

    Improve root index.php and htaccess for cPanel

--- index.php
+++ index.php
@@ -2,11 +2,24 @@
 
 /**
  * Laravel - A PHP Framework For Web Artisans
- * This file redirects all requests to public/index.php
+ * Root index file that forwards to public/index.php
  */
 
-// Change to the public directory
-chdir(__DIR__ . '/public');
+use Illuminate\Foundation\Application;
+use Illuminate\Http\Request;
 
-// Include the public index.php
-require __DIR__ . '/public/index.php';
+define('LARAVEL_START', microtime(true));
+
+// Determine if the application is in maintenance mode...
+if (file_exists($maintenance = __DIR__.'/storage/framework/maintenance.php')) {
+    require $maintenance;
+}
+
+// Register the Composer autoloader...
+require __DIR__.'/vendor/autoload.php';
+
+// Bootstrap Laravel and handle the request...
+/** @var Application $app */
+$app = require_once __DIR__.'/bootstrap/app.php';
+
+$app->handleRequest(Request::capture());
