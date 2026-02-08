feca54564c1701265c378a516c726316ccb936e5
novislab <novislab.dev@gmail.com>
2026-02-08 03:08:52 +0100

    Update nginx config with domain tapnexa.pro

--- provision.sh
+++ provision.sh
@@ -33,21 +33,34 @@ systemctl enable nginx
 cat > /etc/nginx/sites-available/laravel <<EOF
 server {
     listen 80;
-    server_name _;
+    listen [::]:80;
+    server_name tapnexa.pro www.tapnexa.pro;
     root /var/www/laravel/public;
+
+    add_header X-Frame-Options "SAMEORIGIN";
+    add_header X-Content-Type-Options "nosniff";
+
     index index.php;
+
+    charset utf-8;
 
     location / {
         try_files \$uri \$uri/ /index.php?\$query_string;
     }
 
-    location ~ \.php$ {
-        include snippets/fastcgi-php.conf;
+    location = /favicon.ico { access_log off; log_not_found off; }
+    location = /robots.txt  { access_log off; log_not_found off; }
+
+    error_page 404 /index.php;
+
+    location ~ ^/index\.php(/|\$) {
         fastcgi_pass unix:/var/run/php/php8.4-fpm.sock;
-        fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
+        fastcgi_param SCRIPT_FILENAME \$realpath_root\$fastcgi_script_name;
+        include fastcgi_params;
+        fastcgi_hide_header X-Powered-By;
     }
 
-    location ~ /\.ht {
+    location ~ /\.(?!well-known).* {
         deny all;
     }
 }
